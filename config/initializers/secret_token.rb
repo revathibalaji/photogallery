dc8bd36b1188b313cca36aa26424e0a8be5fc27e
FixNix <fixnix3@ubuntu.(none)>
2014-01-18 17:50:09 +0530

    photo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photo::Application.config.secret_key_base = '2841a69a7486b3edf44643b501ab94aa11c081289dabf403a487ac9ebbbff33c2ff38afde341d9dd335d3ab181695d8015f5f7abb0125a593e22e2d89b5380d7'
